9cf628bfe6e6c145b8c90a368e7b7f893a715d92
andrew-james-yoti <andrew.james@yoti.com>
2019-08-13 09:05:05 +0100

    added regenerator runtime

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import 'regenerator-runtime/runtime';
 import App from './components/app';
-import 'regenerator-runtime';
 
 ReactDom.render(<App/>, document.getElementById('root'));
